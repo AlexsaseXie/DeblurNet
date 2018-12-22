f471e1f39ad949720db937b19d16bb57591c3a00
shi-gy15 <vinwrau@163.com>
2018-12-22 09:52:55 +0800

    DecoderBlock

--- src/models/models.py
+++ src/models/models.py
@@ -7,19 +7,15 @@ class ResBlock(nn.Module):
     # conv -> norm -> relu -> conv
     def __init__(self, in_channels, out_channels, kernel_size: int, stride=1):
         super(ResBlock, self).__init__()
-        self.pad1 = nn.ReflectionPad2d(kernel_size // 2)
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=kernel_size // 2)
         self.norm = nn.InstanceNorm2d(out_channels)
-        self.relu = nn.ReLU(True)
-        self.pad2 = nn.ReflectionPad2d(kernel_size // 2)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=kernel_size // 2)
 
         self.conv_network = nn.Sequential((
-            self.pad1,
             self.conv1,
             self.norm,
             self.relu,
-            self.pad2,
             self.conv2
         ))
 
@@ -28,18 +24,19 @@ class ResBlock(nn.Module):
         return out
 
 
-class InputBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size: int, stride=1):
-        super(InputBlock, self).__init__()
-        self.pad1 = nn.ReflectionPad2d(kernel_size // 2)
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0)
-        self.res1 = ResBlock(out_channels, out_channels, kernel_size, stride)
-        self.res2 = ResBlock(out_channels, out_channels, kernel_size, stride)
-        self.res3 = ResBlock(out_channels, out_channels, kernel_size, stride)
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size: int = 5, stride=2):
+        super(EncoderBlock, self).__init__()
+        self.stride = stride
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=self.stride, padding=kernel_size // 2)
+        self.relu = nn.ReLU()
+        self.res1 = ResBlock(out_channels, out_channels, kernel_size, self.stride)
+        self.res2 = ResBlock(out_channels, out_channels, kernel_size, self.stride)
+        self.res3 = ResBlock(out_channels, out_channels, kernel_size, self.stride)
 
         self.network = nn.Sequential((
-            self.pad1,
-            self.conv1,
+            self.conv,
+            self.relu,
             self.res1,
             self.res2,
             self.res3
@@ -50,21 +47,49 @@ class InputBlock(nn.Module):
         return out
 
 
-class EncoderBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size: int, stride=2):
-        super(EncoderBlock, self).__init__()
-        self.pad1 = nn.ReflectionPad2d(kernel_size // 2)
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0)
-        self.res1 = ResBlock(out_channels, out_channels, kernel_size, stride)
+class InputBlock(EncoderBlock):
+    def __init__(self, in_channels, out_channels, kernel_size: int = 5, stride=1):
+        super(InputBlock, self).__init__(in_channels, out_channels, kernel_size, stride)
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size: int = 4, stride=2):
+        super(DecoderBlock, self).__init__()
+        self.stride = stride
+        self.padding = (self.stride * (in_channels - 1) - 2 * in_channels + kernel_size) // 2
+        self.res1 = ResBlock(in_channels, out_channels, kernel_size, stride)
         self.res2 = ResBlock(out_channels, out_channels, kernel_size, stride)
         self.res3 = ResBlock(out_channels, out_channels, kernel_size, stride)
+        self.deconv = nn.ConvTranspose2d(out_channels, out_channels, kernel_size=kernel_size, stride=self.stride, padding=self.padding)
+        self.relu = nn.ReLU()
 
         self.network = nn.Sequential((
-            self.pad1,
-            self.conv1,
             self.res1,
             self.res2,
-            self.res3
+            self.res3,
+            self.deconv,
+            self.relu
+        ))
+
+    def forward(self, x):
+        out = self.network(x)
+        return out
+
+
+class OutputBlock(nn.Module):
+    # no activation at last
+    def __init__(self, in_channels, out_channels, kernel_size: int = 5, stride=1):
+        super(OutputBlock, self).__init__()
+        self.res1 = ResBlock(in_channels, out_channels, kernel_size, stride)
+        self.res2 = ResBlock(out_channels, out_channels, kernel_size, stride)
+        self.res3 = ResBlock(out_channels, out_channels, kernel_size, stride)
+        self.conv = nn.Conv2d(out_channels, out_channels, kernel_size, stride=stride, padding=kernel_size // 2)
+
+        self.network = nn.Sequential((
+            self.res1,
+            self.res2,
+            self.res3,
+            self.conv
         ))
 
     def forward(self, x):
